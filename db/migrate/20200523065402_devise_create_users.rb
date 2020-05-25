db003923e3dfb24b1013438f23a746a25a5f5859
USER <user@USERnoMacBook-ea.local>
2020-05-25 15:17:39 +0900

    sing up 作製開始

--- db/migrate/20200523065402_devise_create_users.rb
+++ db/migrate/20200523065402_devise_create_users.rb
@@ -31,12 +31,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
-
-      t.string :username
-      t.text :profile
-      t.string :profile_image_id
-      t.timestamps null: false
-      t.timestamps null: false
     end
 
     add_index :users, :email,                unique: true
